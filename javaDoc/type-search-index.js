009927a2b63b61c81f1c12c1dbccce0decb3dd4e
EDM115 <dev@edm115.eu.org>
2023-01-20 13:39:40 +0100

    Grundy2

--- javaDoc/type-search-index.js
+++ javaDoc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Grundy2RecBruteEff"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Grundy2RecPerdEtGagn"}];updateSearchResults();
